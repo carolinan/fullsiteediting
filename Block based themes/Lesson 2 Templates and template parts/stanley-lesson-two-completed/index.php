431022237c82072f2ce1fbbf4385be8710c7a85b
Carolina Nymark <hi@themesbycarolina.com>
2021-02-13 11:45:20 +0100

    Update

--- Block based themes/Lesson 2 Templates and template parts/stanley-lesson-two-completed/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This file is here to prevent breaking.
- *
- * @package Stanley Lesson Two Completed
- * @since 1.0.0
- */
-
-_e( 'Enable full site editing to view this theme.', 'stanley-lesson-two-completed' );
--- Block based themes/Lesson 2 Templates and template parts/stanley-lesson-two/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This file is here to prevent breaking.
- *
- * @package Stanley Lesson Two
- * @since 1.0.0
- */
-
-_e( 'Enable full site editing to view this theme.', 'stanley-lesson-two' );

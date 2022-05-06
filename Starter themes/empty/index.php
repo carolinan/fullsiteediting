d22b3522e3fc39c9a84fea196622a35847361cb5
Carolina Nymark <myazalea@hotmail.com>
2022-05-06 15:59:12 +0200

    Update templates and index.htrml

--- Starter themes/empty/index.php
+++ Starter themes/empty/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This file has been left empty on purpose.
- * It is required by WordPress for the theme to activates.
+ * It is required by WordPress 5.8 and 5.9 for the theme to activate.
  *
  * @link https://core.trac.wordpress.org/ticket/54272
  *

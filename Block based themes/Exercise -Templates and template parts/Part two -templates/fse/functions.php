4c2e5acf52b40455a862b949e726e821463d0cdd
Carolina Nymark <myazalea@hotmail.com>
2022-05-20 13:40:34 +0200

    Update functions.php

--- Block based themes/Exercise -Templates and template parts/Part two -templates/fse/functions.php
+++ Block based themes/Exercise -Templates and template parts/Part two -templates/fse/functions.php
@@ -17,10 +17,9 @@ function fse_styles() {
 	wp_enqueue_style(
 		'fse-style',
 		get_stylesheet_uri(),
-		'',
+		array(),
 		wp_get_theme()->get( 'Version' )
 	);
-
 }
 add_action( 'wp_enqueue_scripts', 'fse_styles' );
 
